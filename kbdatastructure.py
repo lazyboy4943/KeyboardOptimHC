54055bc0306a4b51519c58b34a485a1a41536aad
lazyboy4943 <lazyboyzone@gmail.com>
2022-07-16 14:19:16 +0800

    Initial commit

---
+++ kbdatastructure.py
@@ -0,0 +1,18 @@
+# Define the data structure for a keyboard layout
+
+'''
+we assume that the only thing that changes are the
+positions of the letters and all the other keys
+like the shift and caps lock keys stay in the
+same place
+'''
+ 
+# so the data structure will be a list of 3 lists representing each row of the keyboard
+# the keyboard layout for the qwerty keyboard would be: 
+
+qwertylayout = [
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['z','x', 'c', 'v', 'b', 'n', 'm'],
+]
+
---
+++ kbefficiencyvalue.py
@@ -0,0 +1,10 @@
+'''
+when typing the index fingers lie on f and j.
+using this hand position, we need to determine
+the distance to that key from the closest finger.
+
+for now we will assume the user of the new 
+keyboard layout is a complete beginner
+
+using data i need to find which key pairs take the longest to type
+'''
